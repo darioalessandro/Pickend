4b1bf33640004f57019e390786dcf8b4914eec93
Dario Lencina <darioalessandrolencina@gmail.com>
2012-02-27 20:42:17 -0500

    Added XCode Project

---
+++ Pickend/PickendTests/PickendTests.h
@@ -0,0 +1,13 @@
+//
+//  PickendTests.h
+//  PickendTests
+//
+//  Created by Dario Lencina on 2/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PickendTests : SenTestCase
+
+@end
---
+++ Pickend/PickendTests/PickendTests.m
@@ -0,0 +1,32 @@
+//
+//  PickendTests.m
+//  PickendTests
+//
+//  Created by Dario Lencina on 2/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PickendTests.h"
+
+@implementation PickendTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PickendTests");
+}
+
+@end
